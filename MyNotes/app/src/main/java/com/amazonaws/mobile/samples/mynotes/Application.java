d92706056802adcd12e303b369a0a4130897e745
Adrian Hall <adrian@shellmonger.com>
2017-08-03 16:38:41 -0700

    Added ActivityLifeCycle class

--- MyNotes/app/src/main/java/com/amazonaws/mobile/samples/mynotes/Application.java
+++ MyNotes/app/src/main/java/com/amazonaws/mobile/samples/mynotes/Application.java
@@ -12,8 +12,10 @@
  */
 package com.amazonaws.mobile.samples.mynotes;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.multidex.MultiDexApplication;
-
+import android.util.Log;
 
 /**
  * Application class responsible for initializing singletons and other
@@ -23,5 +25,53 @@ public class Application extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifeCycle());
+
+    }
+}
+
+class ActivityLifeCycle implements android.app.Application.ActivityLifecycleCallbacks {
+    private int depth = 0;
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        if (depth == 0) {
+            Log.d("ActivityLifeCycle", "Application entered foreground");
+        }
+        depth++;
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        depth--;
+        if (depth == 0) {
+            Log.d("ActivityLifeCycle", "Application entered background");
+        }
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
     }
 }
